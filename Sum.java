f8b3fbd884f158140db4cd034555818e7e1d42ab
RoshniMariaShaji <84896587+RoshniMariaShaji@users.noreply.github.com>
2022-01-08 12:44:03 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Sum
+{
+	public static void main(String args[])
+	{
+		System.out.println("Enter two numbers:");
+		Scanner obj=new Scanner(System.in);
+		int a=obj.nextInt();
+		int b=obj.nextInt();
+		int sum=a+b;
+		System.out.println("Sum is "+sum);
+	}
+}
